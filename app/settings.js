60dcfef7d708d0414cad1abe2a1aec689d272b88
mtoko <darryl_peacock@hotmail.com>
2018-08-08 13:37:35 +0100

    Add files via upload

--- app/settings.js
+++ app/settings.js
@@ -118,7 +118,7 @@ module.exports = {
     // the static content (httpStatic), the following properties can be used.
     // The pass field is a bcrypt hash of the password.
     // See http://nodered.org/docs/security.html#generating-the-password-hash
-    //httpNodeAuth: {user:"user",pass:"$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN."},
+    httpNodeAuth: {user:"admin",pass:"$2y$12$hozfa2L9gXVDewpi2R/HPuqD/LasMDw01VPZ10CTcxfeP3xcbWhhK."},
     //httpStaticAuth: {user:"user",pass:"$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN."},
 
     // The following property can be used to enable HTTPS
